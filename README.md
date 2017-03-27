8900cbe801ddbb6d2eb3a7fc11fbcfd56e388558
hongxuanlee <lihongxuan213@gmail.com>
2017-03-27 22:22:42 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 ### simple raw socket demo
 
 It's only use for linux, I'm still not check in OSX.
+
+
+### usage
+
+- compile
+
+```
+  gcc main.c -o main
+```
+
+- set proxy_ip
+
+```
+  #define PROXY_IP "100.69.167.224"  // your proxt dev ip
+```
+
+- run 
+
+```
+  ./main 115.239.211.112  // forward ip to real http server, this ip is baidu.com
+```
+
+- curl in another machine， now you will recieve result, and every ip packet will through your proxy dev.
+
+```
+  curl PROXY_IP:55555
+```
+
+### iptables
+
+if you use tcpdump , and despect proxy dev alway send RST to request source, set iptables below：
+
+```
+sudo iptables -A OUTPUT -p tcp -m tcp --tcp-flags RST RST -j DROP
+```
+
+when your end your demo , then move it by:
+
+```
+sudo iptables -D OUTPUT -p tcp -m tcp --tcp-flags RST RST -j DROP
+
+```
+
+
--- main.c
+++ main.c
@@ -7,13 +7,19 @@
 #include <arpa/inet.h>
 #include "print.c"
 
-#define SOURCE_IP "11.164.8.106"
-
 #define PROXY_IP "100.69.167.224"
 
-#define DEST_IP "30.8.70.185"
+#define PROXY_INPORT 55555 // in
 
-#define DEST_PORT 3000
+#define PROXY_OUTPORT 55556 // out
+
+u_int32_t source_ip;
+
+u_int16_t source_port;
+
+char dest_ip[32];
+
+int dest_port = 80;
 
 struct pseudoHeader
 {
@@ -24,10 +30,40 @@ struct pseudoHeader
     u_int16_t tcp_len;
 };
 
-int ip_match(u_int32_t addr1, u_int32_t addr2)
+
+void str_replace(char *target, const char *needle, const char *replacement)
 {
-    int res = !(addr1 ^ addr2);
-    return res;
+    char buffer[8064] = { 0 };
+    char *insert_point = &buffer[0];
+    const char *tmp = target;
+    size_t needle_len = strlen(needle);
+    size_t repl_len = strlen(replacement);
+
+    while (1) {
+        const char *p = strstr(tmp, needle);
+
+        if (p == NULL) {
+            strcpy(insert_point, tmp);
+            break;
+        }
+
+        memcpy(insert_point, tmp, p - tmp);
+        insert_point += p - tmp;
+
+        memcpy(insert_point, replacement, repl_len);
+        insert_point += repl_len;
+
+        tmp = p + needle_len;
+    }
+
+    strcpy(target, buffer);
+}
+
+char *handlePayload(char* buffer){
+    char host_str[80];
+    memset(host_str, 0, 80);
+    sprintf(host_str, "Host: %s:%d", dest_ip, dest_port);
+    str_replace(buffer, "Host: 100.69.167.224:55555", host_str);
 }
 
 unsigned short csum(unsigned short *ptr,int nbytes) 
@@ -84,24 +120,39 @@ unsigned short tcpCheckSum(struct iphdr* iph, struct tcphdr* tcph, char* data, i
 
 void ProcessPacket(unsigned char* buffer, int size, int s){
     struct iphdr *iph = (struct iphdr *)buffer;
-    int iphdrlen = iph->ihl*4;
+    unsigned short iphdrlen = iph->ihl*4;
     struct tcphdr *tcph=(struct tcphdr*)(buffer + iphdrlen);
+    unsigned short tcphdrlen = tcph->doff*4;
+    unsigned char *payload = buffer + iphdrlen + tcphdrlen;
     struct sockaddr_in src, dst;
     src.sin_addr.s_addr = iph->saddr;
     dst.sin_addr.s_addr = iph->daddr; 
     u_int32_t source = iph -> saddr;      
     u_int32_t dest = iph -> daddr;
-    if(ip_match(source, inet_addr(SOURCE_IP)))
+    if(ntohs(tcph -> dest) == PROXY_INPORT)
     {
+        source_port = tcph -> source;
+        source_ip = iph -> saddr;
         iph -> saddr = inet_addr(PROXY_IP); 
-        iph -> daddr = inet_addr(DEST_IP); 
-        tcph -> dest = htons(3000); 
-    }else if(ip_match(source, inet_addr(DEST_IP)) && 22 != ntohs(tcph->dest))
+        iph -> daddr = inet_addr(dest_ip); 
+        tcph -> source = htons(PROXY_OUTPORT);
+        tcph -> dest = htons(dest_port); 
+        unsigned short data_len = ntohs(iph -> tot_len) - iphdrlen - tcphdrlen; 
+        printf("data_len: %d \n", data_len);
+        if(data_len > 0){
+          printf("old data_len: %d\n", strlen(payload));
+          handlePayload(payload);
+          printf("new data_len: %d\n", strlen(payload));
+          iph -> tot_len = htons(iphdrlen + tcphdrlen + strlen(payload));
+        }
+   }
+    else if(ntohs(tcph -> dest) == PROXY_OUTPORT)
     {
-        printf("___recieve back!!!\n");
-        iph -> daddr = inet_addr(SOURCE_IP); 
+        printf("recieve back!!!\n");
+        iph -> daddr = source_ip; 
         iph -> saddr = inet_addr(PROXY_IP); 
-        tcph -> source = htons(8877);
+        tcph -> dest = source_port;
+        tcph -> source = htons(PROXY_INPORT);
     }else
     {
         return;
@@ -110,10 +161,9 @@ void ProcessPacket(unsigned char* buffer, int size, int s){
     printf("Destination IP   : %s\n",inet_ntoa(dst.sin_addr));
 
     iph -> check = ipCheckSum(iph, tcph, buffer, size);              
-    unsigned short tcphdrlen = tcph->doff*4;
-    tcph -> check = tcpCheckSum(iph, tcph, buffer + iphdrlen + tcphdrlen, (size - iphdrlen - tcphdrlen));
+    tcph -> check = tcpCheckSum(iph, tcph, payload, strlen(payload));
     print_tcp_packet(buffer, iph, tcph, size);  
-    
+
     struct sockaddr_in dest_addr;
     dest_addr.sin_family = AF_INET;
     dest_addr.sin_port = tcph -> dest;
@@ -130,8 +180,17 @@ void ProcessPacket(unsigned char* buffer, int size, int s){
 }
 
 int main(int argc, const char * argv[]) {
+    if(argc < 2){
+        printf("arguments missing");
+        exit(-2); 
+    }
+    memset(dest_ip, 0, 32);
+    strcpy(dest_ip, argv[1]);
+    if(argc > 2){
+      dest_port = strtol(argv[2], NULL, 10);
+    }
     printf("main start \n");
-   //Create a raw socket
+    //Create a raw socket
     int s = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);
     if(s == -1)
     {
@@ -141,21 +200,17 @@ int main(int argc, const char * argv[]) {
     const int *val = &one;
     struct sockaddr_in serverProxy;
     serverProxy.sin_family = AF_INET;
-    serverProxy.sin_port = htons(DEST_PORT);
     serverProxy.sin_addr.s_addr = inet_addr(PROXY_IP);
     if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
     {
         perror("Error setting IP_HDRINCL");
     }
-    // bind port
-      bind(s, (struct sockaddr *)&serverProxy, sizeof(serverProxy));
 
     while(1) {
-        u_char buffer[1024];
-
-        u_char raw_Buffer[1024];
+        u_char raw_Buffer[8064];
+        memset (raw_Buffer, 0, 8064);
         int saddr_size = sizeof serverProxy;
-        int data_size = recvfrom(s , raw_Buffer , 1024, 0 , (struct sockaddr *)&serverProxy , &saddr_size);
+        int data_size = recvfrom(s , raw_Buffer , 8064, 0 , (struct sockaddr *)&serverProxy , &saddr_size);
 
         if(data_size < 0)
         {
